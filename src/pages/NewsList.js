f7deca84dd4dd0f14abf62937060eb2989610fbf
grace chuang <grace350276@gmail.com>
2022-03-11 15:11:06 +0800

    最新消息_測試階段

--- src/pages/NewsList.js
+++ src/pages/NewsList.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import  './../styles/news/news.css'
 
 const NewsList = () => {
   return (
-    <div>NewsList</div>
+    <>
+    <div className="newstry">NewsList nav bar</div>
+    <div className="container">這裡container為什麼沒有連動到BS</div>
+
+    <h1>H1 文字示意</h1>
+    <h2>H2 文字示意</h2>
+
+
+    
+    </>
+
   )
 }
 
