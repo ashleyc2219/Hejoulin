cf145cd9c2dd4aae046d5cc70deb64f67ab80f28
Ashley Chen <ashleyc2219@gmail.com>
2022-03-11 10:38:57 +0800

    color btn input 說明

--- src/compenents/SomeComponent.js
+++ src/compenents/SomeComponent.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const SomeComponent = () => {
   return (
-    <div>SomeComponent</div>
+    <div>
+      <h1>Some Components</h1>
+      {/* 按鈕 */}
+      <h3>按鈕們</h3>
+      <button className='btn btn-primary'>Primary</button>
+      <button className='btn btn-secondary'>Secondary</button>
+      <button className='btn btn-primary-outline'>Outline</button>
+      <br/>
+      <h4>不同大小</h4>
+      <p>btn-sm是style guide中的small</p>
+      <p>btn是style guide中的normal</p>
+      <p>btn-large是style guide中的medium</p>
+      <button className='btn btn-sm btn-primary'>btn-sm</button>
+      <button className='btn btn-primary'>btn</button>
+      <button className='btn btn-lg btn-primary'>btn-large</button>
+      {/* 輸入欄 */}
+      <h3>input</h3>
+      <div className="mb-3">
+      <label className="form-label">標籤</label>
+      <input type="email" className="form-control" placeholder='示意文字' />
+      <div className="form-text">錯誤/提示訊息</div>
+    </div>
+    </div>
   )
 }
 
