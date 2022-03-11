5b01c7d7bf2804f46d98377018089bda7f440da8
Mary Hsieh <a5925457@gmail.com>
2022-03-11 09:38:06 +0800

    setting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 
 const Home = () => {
-  return (
-    <div>
-    <div class="alert alert-primary" role="alert">
-    A simple primary alert—check it out!
-    </div>
-    <Button variant="primary">
-          Save Changes
-        </Button>
-    </div>
-  )
-}
+    return (
+        <div>
+            <div class="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+            <Button variant="primary">Save Changes</Button>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
