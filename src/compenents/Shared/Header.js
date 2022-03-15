c19ccf5deb0b9f1c27ba8c443782762d5d032310
Ashley Chen <ashleyc2219@gmail.com>
2022-03-15 15:38:06 +0800

    header靜態版

--- src/compenents/Shared/Header.js
+++ src/compenents/Shared/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import './../../styles/Shared/Header.scss'
 
 const Header = () => {
@@ -10,30 +11,51 @@ const Header = () => {
             <img className="logo" src="/Shared/logo.svg" alt="" />
             <div className="bread">
               <span>首頁</span>
-              <img src="/Shared/rightArrow.svg" />
+              <img src="/Shared/rightArrow.svg" alt="" />
               <span className="current">購物車</span>
             </div>
           </div>
           <div className="navbar">
-            <div className="links">
-              <a>最新消息</a>
-              <a>認識清酒</a>
-              <div class="purchase-sake">
-                <button class="dropdown-btn">購買清酒</button>
-                <div class="dropdown-content">
-                  <a href="">選購清酒</a>
-                  <a href="">選酒指南</a>
-                  <a href="">禮盒系列</a>
-                </div>
-              </div>
+            <ul className="links">
+              <li>
+                <Link to="">最新消息</Link>
+              </li>
+              <li>
+                <Link to="">認識清酒</Link>
+              </li>
+              <li className="purchase-sake">
+                <Link to="">購買清酒</Link>
+                <ul className="dropdown-content">
+                  <li href="">
+                    <Link to="">選購清酒</Link>
+                  </li>
+                  <li href="">
+                    <Link to="">選酒指南</Link>
+                  </li>
+                  <li href="">
+                    <Link to="">禮盒系列</Link>
+                  </li>
+                </ul>
+              </li>
 
-              <a>客製化酒標</a>
-              <a>訂閱清酒</a>
-              <a>參加活動</a>
-              <a>合作餐廳</a>
-            </div>
+              <li>
+                <Link to="">客製化酒標</Link>
+              </li>
+              <li>
+                <Link to="">訂閱清酒</Link>
+              </li>
+              <li>
+                <Link to="">參加活動</Link>
+              </li>
+              <li>
+                <Link to="">合作餐廳</Link>
+              </li>
+            </ul>
             <div className="icons">
-              <img className="login" alt="" src="/Shared/loginIcon.svg" />
+              <div className="login">
+                <img className="" alt="" src="/Shared/loginIcon.svg" />
+                <span>登入</span>
+              </div>
               <img className="cart" alt="" src="/Shared/shoppingCart.svg" />
               <img className="fav" alt="" src="/Shared/heart.svg" />
             </div>
