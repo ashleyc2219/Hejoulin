2ac2dbcff0b03d5bd28db2bcb9194627cbfd3f7d
grace chuang <grace350276@gmail.com>
2022-03-16 15:21:13 +0800

    最新消息 背景 文字方塊

--- src/pages/NewsList.js
+++ src/pages/NewsList.js
@@ -1,19 +1,39 @@
 import React from 'react'
-import  './../styles/news/news.css'
+import './../styles/news/news.css'
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
+import SomeComponent from '../compenents/SomeComponent'
+import Header from '../compenents/Shared/Header'
+import Footer from '../compenents/Shared/Footer'
 
 const NewsList = () => {
   return (
     <>
-    <div className="newstry">NewsList nav bar</div>
-    <div className="container">這裡container為什麼沒有連動到BS</div>
-
-    <h1>H1 文字示意</h1>
-    <h2>H2 文字示意</h2>
-
-
-    
+      <Header />
+      <div className="news">
+        <div className="news-container">
+          <div className="news-title">
+            <h1>最新消息</h1>
+          </div>
+          <div className="news-item">
+            <div className="news-item-word">
+              <div className="news-item-date">2022-01-04</div>
+              <div className="news-item-title">
+                虎年最有「禮」，新年送禮限量折扣
+              </div>
+              <div className="news-item-content">
+                又到了歲末年終表達心意的時節，從年節限定、華麗包裝、超值贈品，甚至還有箱購優惠，絕對能滿足...
+              </div>
+              <div className="news-item-link">
+                <a href="#">了解詳情...</a>
+              </div>
+            </div>
+            <div className="news-item pic">
+              <div className="news-pic"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-
   )
 }
 
