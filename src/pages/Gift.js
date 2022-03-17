671d898f56334493943bf4a02d93330f94b09660
Winnie Chien <winnie145ch@gmail.com>
2022-03-17 16:59:34 +0800

    gift

--- src/pages/Gift.js
+++ src/pages/Gift.js
@@ -11,10 +11,10 @@ const Gift = () => {
     <div className="Gift">
       <div className="gift_container">
         {/* background-patten */}
-        {/* <img src="/Gift/bgelement.svg" alt="" className="bgelement01" />
+        <img src="/Gift/bgelement.svg" alt="" className="bgelement01" />
         <img src="/Gift/bgelement.svg" alt="" className="bgelement02" />
         <img src="/Gift/bgelement.svg" alt="" className="bgelement03" />
-        <img src="/Gift/bgelement.svg" alt="" className="bgelement04" /> */}
+        <img src="/Gift/bgelement.svg" alt="" className="bgelement04" />
         <Sidebar />
 
         {/*內容*/}
@@ -49,7 +49,8 @@ const Gift = () => {
               <div className="grid gift_grid">
                 <div className="gift_sake">
                   <div className="col sake_card">
-                    <img src="/Gift/4.png" alt="" />
+                    <img src="/Gift/4.png" alt="" className="original" />
+                    <img src="/Gift/1.png" alt="" className="trans" />
                     <span className="name">水芭蕉　鼠年限定</span>
                     <small>$2890</small>
                   </div>
@@ -58,7 +59,8 @@ const Gift = () => {
                 </div>
                 <div className="gift_sake">
                   <div className="col sake_card">
-                    <img src="/Gift/4.png" alt="" />
+                    <img src="/Gift/4.png" alt="" className="original" />
+                    <img src="/Gift/1.png" alt="" className="trans" />
                     <span className="name">水芭蕉　鼠年限定</span>
                     <small>$2890</small>
                   </div>
@@ -67,7 +69,8 @@ const Gift = () => {
                 </div>
                 <div className="gift_sake">
                   <div className="col sake_card">
-                    <img src="/Gift/4.png" alt="" />
+                    <img src="/Gift/4.png" alt="" className="original" />
+                    <img src="/Gift/1.png" alt="" className="trans" />
                     <span className="name">水芭蕉　鼠年限定</span>
                     <small>$2890</small>
                   </div>
@@ -76,7 +79,8 @@ const Gift = () => {
                 </div>
                 <div className="gift_sake">
                   <div className="col sake_card">
-                    <img src="/Gift/4.png" alt="" />
+                    <img src="/Gift/4.png" alt="" className="original" />
+                    <img src="/Gift/1.png" alt="" className="trans" />
                     <span className="name">水芭蕉　鼠年限定</span>
                     <small>$2890</small>
                   </div>
@@ -85,7 +89,8 @@ const Gift = () => {
                 </div>
                 <div className="gift_sake">
                   <div className="col sake_card">
-                    <img src="/Gift/4.png" alt="" />
+                    <img src="/Gift/4.png" alt="" className="original" />
+                    <img src="/Gift/1.png" alt="" className="trans" />
                     <span className="name">水芭蕉　鼠年限定</span>
                     <small>$2890</small>
                   </div>
@@ -94,7 +99,8 @@ const Gift = () => {
                 </div>
                 <div className="gift_sake">
                   <div className="col sake_card">
-                    <img src="/Gift/4.png" alt="" />
+                    <img src="/Gift/4.png" alt="" className="original" />
+                    <img src="/Gift/1.png" alt="" className="trans" />
                     <span className="name">水芭蕉　鼠年限定</span>
                     <small>$2890</small>
                   </div>
@@ -109,15 +115,15 @@ const Gift = () => {
               </div>
               <h4>請選擇禮盒顏色</h4>
               <div className="color_group">
-                <button className="btn color_btn">
+                <button className="color_btn">
                   <img src="/Gift/black.svg" alt="" />
                   <small>曜岩黑</small>
                 </button>
-                <button className="btn color_btn">
+                <button className="color_btn">
                   <img src="/Gift/white.svg" alt="" />
                   <small>英石白</small>
                 </button>
-                <button className="btn color_btn">
+                <button className="color_btn">
                   <img src="/Gift/golden.svg" alt="" />
                   <small>流沙金</small>
                 </button>
@@ -127,17 +133,42 @@ const Gift = () => {
                 <img src="/Gift/gift.png" alt="sake" className="sake" />
                 <img src="/Gift/gift.png" alt="container" className="other" />
               </div>
+              <div className="confirm">
+                <button className="btn btn-sm btn-primary">下一步</button>
+              </div>
             </section>
             <section id="gift_detail">
+              <div className="header">
+                <img src="/Gift/bgmark.svg" alt="" className="bgmark" />
+              </div>
               <h4>請確認禮盒明細數量</h4>
               <div className="detail">
-                <img src="#" alt="final" />
+                <img src="/Gift/3.png" alt="final" className="final" />
                 <div className="content">
-                  <h5>禮盒明細</h5>
-                  <div className="text">水芭蕉　牛年限定</div>
-                  <div>$2780</div>
-                  <h5>禮盒數量</h5>
-                  <div className="add-minus">
+                  <h5 className="title">禮盒明細</h5>
+                  <div className="sheet grid">
+                    {/* gift sake */}
+                    <p className="text color">水芭蕉　牛年限定</p>
+                    <p className="quality color">1瓶</p>
+                    <div className="price color">780</div>
+                    <p className="text color">陶器豬口杯和清酒壺</p>
+                    <p className="quality color">1組</p>
+                    <div className="price color">600</div>
+                    {/* space */}
+                    <p></p>
+                    <p></p>
+                    <div></div>
+                    {/* gift color */}
+                    <p className="text color">禮盒顏色</p>
+                    <p className="quality color">曜岩黑</p>
+                    <div className="price color">200</div>
+                    {/* gift total */}
+                    <p className="text_total total">總價</p>
+                    <p className="quality total">1盒</p>
+                    <div className="price total">2780</div>
+                  </div>
+                  <h5 className="laststep">禮盒數量</h5>
+                  <div className="add-minus row">
                     <div className="minus">
                       <img src="/Gift/minus-circle.svg" alt="" />
                     </div>
@@ -146,7 +177,10 @@ const Gift = () => {
                       <img src="/ProductList/plus-circle.svg" alt="" />
                     </div>
                   </div>
-                  <button className="btn btn-primary btn-sm">加入購物車</button>
+                  <button className="btn btn-primary btn-sm gift_cart">
+                    <img src="/Gift/cart.svg" alt="" className="cart" />
+                    加入購物車
+                  </button>
                 </div>
               </div>
             </section>
