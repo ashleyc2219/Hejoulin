4fa222633ebf9f05024af154636aba9d8ff53941
Ashley Chen <ashleyc2219@gmail.com>
2022-03-18 01:14:23 +0800

    波紋動畫 01版

--- src/compenents/Sub/SubPlanCard.js
+++ src/compenents/Sub/SubPlanCard.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './SubPlanCard.scss'
+import { motion, useAnimation } from 'framer-motion'
+import { useState } from 'react'
 
-const SubPlanCard = () => {
+const SubPlanCard = (props) => {
+  const controls = useAnimation()
+
+  function onTap(event, info) {
+    controls.start({
+      d: [
+        'M220 100.786H0V55.689c11.5-2 46.18-4.552 96 7.597C114.056 67.689 202.5 69 220 66.5v34.286z',
+        'M220 100.972H0V51c43 3.5 34.5 24.686 100.5 5.5C131.896 47.373 174 31 220 43v57.972z',
+        'M220 100.972H0V19.265c36.5-12 84.5 12.5 116.5 18.5C129 40.108 193 50.5 220 28v72.972z',
+        'M220 101.972H0V1c71 33 68.576 8.827 126.5 4 12-1 79.5 5.735 93.5 21v75.972z',
+      ],
+    })
+  }
+
   return (
-    <div className="planChoice-card">
+    <motion.div className="planChoice-card" onTap={onTap}>
       <div className="planImg-container">
         <img src="/Sub/riceOne.svg" alt="" />
       </div>
@@ -12,34 +27,30 @@ const SubPlanCard = () => {
         <p>精米步合度約70%</p>
         <p>1200</p>
       </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="209"
-        height="101"
-        fill="none"
-        viewBox="0 0 209 101"
-      >
-        <path
-          fill="#E6C49E"
-          d="M209 12.5C66 22 95.5-8.5 0 3v45h209V12.5z"
-          opacity=".25"
-        >
-          <animate
-            repeatCount="1"
-            attributeName="d"
-            dur="5s"
-            fill="freeze"
-            values="
 
-            M209 .5C74.5.5 113 36 0 13.5V60h209V.5z;
-            M209 3.5c-51.5 50-97.5-18-209 0V85h209V3.5z;
-            M209 19.5C85.135-28.5 100.807 49.5 0 0v101h209V19.5z;
-            
-            "
-          ></animate>
-        </path>
+      <svg
+        style={{
+          width: 209,
+          height: 101,
+        }}
+      >
+        <motion.path
+          // initial={false}
+          style={{
+            fill: '#E6C49E',
+            opacity: '.25',
+          }}
+          d=""
+          // onTap={onTap}
+          animate={controls}
+          transition={{
+            repeat: 0,
+            // ease: 'easeInOut',
+            duration: 2,
+          }}
+        />
       </svg>
-    </div>
+    </motion.div>
   )
 }
 
