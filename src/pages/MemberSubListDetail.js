7c666d20f3d7a4944e7d579e64b666e02a417e65
Wilson#122 <cwhth2956@gmail.com>
2022-03-17 09:39:38 +0800

    完成靜態版活動記錄、訂閱詳細頁、收藏商品頁

--- src/pages/MemberSubListDetail.js
+++ src/pages/MemberSubListDetail.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Sidebar from '../compenents/Member/Sidebar'
+import '../styles/Member/MemberSubListDetail.scss'
 
 const MemberSubListDetail = () => {
   return (
-    <div>MemberSubListDetail</div>
+    <>
+      <div className="MemberSubListDetail">
+        <Sidebar />
+        <div className="MemberSubListDetailBox">
+          <div className="subPlanImg">
+            <img src="/Member/SubPlanEX.svg" alt="" />
+          </div>
+          <div className="subPlanDetail">
+            <div className="detailLeft">
+              <p>訂閱訂單編號 : </p>
+              <p>方案時間 : </p>
+              <p>訂閱單月價格 : </p>
+              <p>訂閱時間週期 : </p>
+            </div>
+            <div className="detailRight">
+              <p>下次帳單日期 : </p>
+              <p>付款方式 : </p>
+              <p>備用付款方式 : </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
