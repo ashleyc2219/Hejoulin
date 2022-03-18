06e3c3c3c92b4a99232cd6cbc060fcbccf5f2b04
Ashley Chen <ashleyc2219@gmail.com>
2022-03-18 12:51:05 +0800

    SubTime (波紋動畫 01版)

---
+++ src/compenents/Sub/SubTimeCard.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './SubTimeCard.scss'
+import { motion, useAnimation } from 'framer-motion'
+import { useState } from 'react'
+
+const SubTimeCard = (props) => {
+  const controls = useAnimation()
+
+  function onTap(event, info) {
+    controls.start({
+      d: [
+        'M220 100.786H0V55.689c11.5-2 46.18-4.552 96 7.597C114.056 67.689 202.5 69 220 66.5v34.286z',
+        'M220 100.972H0V51c43 3.5 34.5 24.686 100.5 5.5C131.896 47.373 174 31 220 43v57.972z',
+        'M220 100.972H0V19.265c36.5-12 84.5 12.5 116.5 18.5C129 40.108 193 50.5 220 28v72.972z',
+        'M220 101.972H0V1c71 33 68.576 8.827 126.5 4 12-1 79.5 5.735 93.5 21v75.972z',
+      ],
+    })
+  }
+
+  return (
+    <motion.div className="timeChoice-card" onTap={onTap}>
+      <div className="timeImg-container">
+        <img src="/Sub/oneMonth.png" alt="" />
+      </div>
+      <div className="timeInfo-container">
+        <p>1</p>
+        <h6>1800</h6>
+        <p>1800</p>
+      </div>
+
+      <svg
+        style={{
+          width: 209,
+          height: 101,
+        }}
+      >
+        <motion.path
+          // initial={false}
+          style={{
+            fill: '#E6C49E',
+            opacity: '.25',
+          }}
+          d=""
+          // onTap={onTap}
+          animate={controls}
+          transition={{
+            repeat: 0,
+            // ease: 'easeInOut',
+            duration: 2,
+          }}
+        />
+      </svg>
+    </motion.div>
+  )
+}
+
+export default SubTimeCard
--- src/pages/SubTime.js
+++ src/pages/SubTime.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import ProgressBar from '../compenents/Cart/ProgressBar'
+import SubTimeCard from '../compenents/Sub/SubTimeCard'
+
+import '../styles/SubTime/SubTime.scss'
 
 const SubTime = () => {
+  const stepContent = ['選擇方案', '選擇週期', '確認方案']
   return (
-    <div>SubTime</div>
+    <div className="SubTime">
+      <img className="LineBg" src="/Sub/LineBg.svg" alt="" />
+
+      <ProgressBar step="two" content={stepContent} />
+      <div className="container">
+        <div className="main">
+          <div className="left-planInfo">
+            <h4>
+              名譽清酒唎酒師 <br />
+              為你精選季節性限定清酒
+            </h4>
+            <hr />
+            <h5>選擇週期</h5>
+            <p>
+              請選擇訂閱週期。
+              <br />
+              越長的訂閱週期越多折扣。
+            </p>
+            <p>
+              你選擇了 <span>純米大吟釀</span>
+            </p>
+          </div>
+          <div className="right-planChoices">
+            <SubTimeCard />
+            <SubTimeCard />
+            <SubTimeCard />
+          </div>
+        </div>
+        <div className='buttons'>
+          <Link to="/sub/plan">
+            <button className="btn btn-primary">上一步</button>
+          </Link>
+          <Link to="/sub/confirm">
+            <button className="btn btn-secondary">下一步</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
