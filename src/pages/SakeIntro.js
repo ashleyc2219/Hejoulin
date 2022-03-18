47baed524282b3ef8e61b50f336e40a0c240bb37
grace chuang <grace350276@gmail.com>
2022-03-18 11:05:17 +0800

    認識清酒 版面

--- src/pages/SakeIntro.js
+++ src/pages/SakeIntro.js
@@ -6,21 +6,53 @@ const SakeIntro = () => {
     <>
       <div className="sake-intro">
         <div className="img-box"></div>
-
-        <div className="container sake-intro-container">
-          <div className="sakeintro-content">
-            <div className="word-box">
-              <p>
-                很多人都認為，清酒要從最貴的開始品飲。因為貴的清酒口感上會比較柔和，也比較好入口。還有人對於清酒的印象停留在「長輩才喝的酒」或「酒精濃度高」，這樣的誤解，也可能是清酒少有被推薦的其中一個原因。我們不妨通過傳遞清酒知識，解開迷思，引導酒友進入清酒的世界。
-              </p>
-            </div>
-            <div className="big-title">
-              <h1>認識清酒</h1>
+        <div className="container">
+          <div className="sake-intro-container">
+            <div className="sakeintro-content">
+              <div className="word-box">
+                <p>
+                  很多人都認為，清酒要從最貴的開始品飲。因為貴的清酒口感上會比較柔和，也比較好入口。還有人對於清酒的印象停留在「長輩才喝的酒」或「酒精濃度高」，這樣的誤解，也可能是清酒少有被推薦的其中一個原因。我們不妨通過傳遞清酒知識，解開迷思，引導酒友進入清酒的世界。
+                </p>
+              </div>
+              <div className="big-title">
+                <h1>認識清酒</h1>
+              </div>
             </div>
           </div>
-          <div className="sakeintro-title1">
+          <div className="sakeintro-title">
             <h2>產地</h2>
             <p>產地對於清酒而言，就是各式不同的組成元素代表。</p>
+          </div>
+
+          <div className="sakeintro-title">
+            <h2>等級</h2>
+            <p>
+              分級標準有二第一樣是<b>「釀造酒精」</b>，而第二樣是
+              <b>「精米步合」。</b>
+            </p>
+          </div>
+
+          <div className="sakeintro-title">
+            <h2>釀造製成</h2>
+            <p>
+              日本酒,葡萄酒和啤酒同屬於釀造酒，其中我們的主角日本酒又有其獨特的發酵作業，現在就讓我們來一步步了解這個國粹的誕生。
+            </p>
+          </div>
+
+          <div className="sakeintro-title">
+            <p>
+              上述流程為傳統上做法，在市面上看到的日本酒不一定完全一致的執行每一步驟，而讓酒質中出現了許多不同特色，但我們能夠確定的是，每支日本酒都是經過漫長的過程和製作者小心呵護之下產出的藝術品。
+            </p>
+          </div>
+
+          <div className="sakeintro-title">
+            <h2>日本酒度酸度</h2>
+            <p>
+              當你要選購琳琅滿目的清酒，一定曾滿腦問號：到底哪瓶清酒才好呢？現在介紹兩種方法，看看能否幫到你。
+              首先，可從酒標上所列的日本酒度及酸度理解酒釀。日本酒度是指酒所含的糖量，較多糖量的會以負值「-」表示，為甘口型，甘即甜的意思，而負值愈大，糖量愈高；較少糖量的會以正值「+」表示，為辛口型，而正值愈大，糖量愈少，「辛」這字，聽上去較難明白，或可以葡萄酒「乾型（Dry）」或香檳的乾型「Brut」的概念理解。
+              同一時間，要考慮的是其酸度，因酸度結合日本酒度，才會真正反映酒釀風格。酸度的基準數為「1.5〜1.7」，若數值高於此，即酸度高，為濃醇型；若數值低於此，即酸度低，為淡麗型。所以，日本清酒常會聽到如「淡麗‧辛口」的形容，意即為酒度低，而含糖少。以來福大吟釀
+              「雫」為例，日本酒度 : +2，即為辛口，酸度 : 1.1，即為淡麗。
+            </p>
           </div>
         </div>
       </div>
