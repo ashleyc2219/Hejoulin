884f19e4fae6a6d705c170c4d3b48716359ba6c6
grace chuang <grace350276@gmail.com>
2022-03-24 14:17:27 +0800

    somecomponent

--- src/pages/NewsDetail.js
+++ src/pages/NewsDetail.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import DetailModal from '../compenents/News/DetailModal'
+import SomeComponent from '../compenents/SomeComponent'
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
 
 const NewsDetail = () => {
   return (
     <>
-      <div>NewsDetail</div>
+      <div>NewsDetail-這頁不需要了 因為Newlist用光箱就好</div>
+      <div>somecomponent放這裡</div>
 
-      <DetailModal />
+      <div>
+        <div className="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+        <Button variant="primary">Save Changes</Button>
+        <SomeComponent />
+      </div>
     </>
   )
 }
