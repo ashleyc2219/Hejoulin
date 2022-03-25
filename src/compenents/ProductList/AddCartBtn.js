ec608942e69ed13bf0b1e68ca8034b500cfed339
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-03-25 13:55:22 +0800

    寫入購物車的元件

---
+++ src/compenents/ProductList/AddCartBtn.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './AddCartBtn.scss'
+import { CartCount } from '../../App'
+
+const AddCartBtn = ({ id, setCartCount, count }) => {
+  const addcart = async (num, id) => {
+    const a = count + num
+    setCartCount(a)
+
+    const data = { member_id: 4, pro_id: `${id}`, cart_quantity: `${count}` }
+    console.log(data)
+    const settings = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+    try {
+      const fetchResponse = await fetch(
+        'http://localhost:3001/api/products-addcart',
+        settings
+      )
+      const data = await fetchResponse.json()
+      if (data.success) {
+        alert('已加入購物車')
+      }
+    } catch (e) {
+      return e
+    } 
+  }
+  return (
+    <>
+      <CartCount.Consumer>
+        {(cartCount) => (
+          <button
+            onClick={() => {
+              addcart(cartCount, id)
+            }}
+            className="btn-sm btn-primary btn cart"
+          >
+            <img src="/ProductList/cart-white.svg" alt="" />
+            加入購物車
+          </button>
+        )}
+      </CartCount.Consumer>
+    </>
+  )
+}
+
+export default AddCartBtn
