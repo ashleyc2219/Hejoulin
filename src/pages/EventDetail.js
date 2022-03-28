9ddcfd9865a009ee63c0ae7019b0295a40b1364b
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-03-28 16:59:14 +0800

    大致完成活動

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './../styles/EventDetail/EventDetail.scss'
+import { useParams } from 'react-router-dom'
 
 const EventDetail = () => {
-  return <div>EventDetail</div>
+  const [data, setData] = useState([])
+  const { id } = useParams()
+  const url = 'http://localhost:3001/api/event?eventId=' + id
+
+  console.log(data)
+
+  const Detail = data.map((v) => {
+    return (
+      <>
+        <div className="main-container">
+          <img
+            src={'http://localhost:3001/images/event_pic/' + v.event_cover}
+            alt=""
+          />
+          <div className="event-info">{v.event_brief}</div>
+        </div>
+        <div className="signup-container">
+          <div className="title">{v.event_name}</div>
+          <div className="dateinfo">
+            <div className="datetitle">日期：</div>
+            <div className="date">
+              {v.event_time_start}{" ~ " +v.event_time_end}
+            </div>
+          </div>
+          <div className="locainfo">
+            <div className="locatitle">地點：</div>
+            <div className="loca">{v.event_location}</div>
+          </div>
+          <div className="price">NT${v.event_cost}</div>
+          <button className="btn btn-primary">立即報名</button>
+        </div>
+      </>
+    )
+  })
+
+  useEffect(() => {
+    let a = true
+
+    const fetchData = async () => {
+      const res = await fetch(url)
+      const data = await res.json()
+      if (a) {
+        setData(data)
+      }
+    }
+    fetchData()
+    return () => {
+      a = false
+    }
+  }, [])
+
+  return (
+    <>
+      <div className="eventDetail">
+        {Detail}
+      </div>
+    </>
+  )
 }
 
 export default EventDetail
