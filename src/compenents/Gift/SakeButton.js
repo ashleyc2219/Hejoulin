28417d94dcef01e68fe802a0d1bcd614621ba36d
Winnie Chien <winnie145ch@gmail.com>
2022-03-29 14:48:56 +0800

    trail

--- src/compenents/Gift/SakeButton.js
+++ src/compenents/Gift/SakeButton.js
@@ -1,8 +1,32 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { Modal, Button } from 'react-bootstrap'
 import './SakeButton.scss'
 
 const SakeButton = (props) => {
+  const {
+    sakeId,
+    setSakeId,
+    sakeId2,
+    setSakeId2,
+    name,
+    setName,
+    name2,
+    setName2,
+    price,
+    setPrice,
+    price2,
+    setPrice2,
+    currentPrice,
+    currentName,
+    currentId,
+    count,
+    setCount,
+  } = props
   const [sakeButton, setSakeButton] = useState(0)
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   const value = useRef(0)
   const plus = () => {
     value.current += 1
@@ -11,15 +35,59 @@ const SakeButton = (props) => {
     value.current -= 1
   }
 
+  let btn = document.querySelectorAll('.sake')
+  const total = () => {
+    switch (value.current) {
+      case 0:
+        // btn.removeAttribute('disable', '')
+        break
+      case 1:
+        // btn.removeAttribute('disable', '')
+        setSakeId(currentId)
+        setName(currentName)
+        setPrice(currentPrice)
+        break
+      case 2:
+        // btn.setAttribute('disable', '')
+        setSakeId2(currentId)
+        setName2(currentName)
+        setPrice2(currentPrice)
+        break
+      default:
+        // handleShow()
+        break
+    }
+  }
+  
   return (
     <>
-      <div className="SakeButton">
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>選購額度已滿！</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>選購清酒額度已滿，請先取消當前已選購的商品</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            確認
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div
+        className="SakeButton"
+        onClick={() => {
+          total()
+          console.log('current:', value.current)
+          console.log(currentId)
+        }}
+      >
         {sakeButton === 0 && (
           <button
             className="sake empty"
-            ref={value}
             onClick={() => {
               setSakeButton(1)
+              setCount(currentId)
+              plus()
+              console.log(count)
             }}
           >
             <img src="/Gift/null_sake.svg" alt="" className="sake single" />
@@ -31,9 +99,11 @@ const SakeButton = (props) => {
           <>
             <button
               className="one"
-              ref={value}
               onClick={() => {
                 setSakeButton(2)
+                // setCount(newCount)
+                plus()
+                // console.log(count)
               }}
             >
               <img src="/Gift/full_sake.svg" alt="" className="sake single" />
@@ -41,9 +111,13 @@ const SakeButton = (props) => {
             </button>
             <button
               className="cancel"
-              ref={value}
               onClick={() => {
                 setSakeButton(0)
+                minus()
+                setSakeId('')
+                setName('')
+                setPrice('')
+                console.log(count)
               }}
             >
               <img src="/Gift/cancel.svg" alt="" className="circle" />
@@ -53,16 +127,25 @@ const SakeButton = (props) => {
         )}
         {sakeButton === 2 && (
           <>
-            <button className="full" ref={value} disable>
+            <button
+              className="full"
+              onClick={() => {
+                handleShow()
+              }}
+            >
               <img src="/Gift/gray_sake.svg" alt="" className="sake dou" />
               <img src="/Gift/gray_sake.svg" alt="" className="sake dou" />
               <span>已選二瓶</span>
             </button>
             <button
               className="cancel"
-              ref={value}
               onClick={() => {
                 setSakeButton(1)
+                minus()
+                setSakeId2('')
+                setName2('')
+                setPrice2('')
+                console.log(count)
               }}
             >
               <img src="/Gift/cancel.svg" alt="" className="circle" />
--- src/compenents/Gift/SakeItems.js
+++ src/compenents/Gift/SakeItems.js
@@ -5,38 +5,96 @@ import SakeButton from './SakeButton'
 import ProductModal from './ProductModal'
 
 const SakeItems = (props) => {
-  const { modalShow, setModalShow, sake } = props
+  const {
+    modalShow,
+    showHandle,
+    closeHandle,
+    sake,
+    id,
+    setId,
+    sakeId,
+    setSakeId,
+    sakeId2,
+    setSakeId2,
+    name,
+    setName,
+    name2,
+    setName2,
+    price,
+    setPrice,
+    price2,
+    setPrice2,
+    conName,
+    setConName,
+    count,
+    setCount,
+  } = props
+  const [currentId, setCurrentId] = useState('')
+  const [currentName, setCurrentName] = useState('')
+  const [currentPrice, setCurrentPrice] = useState('')
 
   const items = sake.map((v, i) => {
     return (
-      <div className="gift_sake" key={i}>
-        <div className="sake_card" onClick={() => setModalShow(!modalShow)}>
-          <img
-            src={'http://localhost:3001/images/pro_img/' + v.pro_img}
-            alt=""
-            className="original"
-          />
-          <img
-            src={'http://localhost:3001/images/con_img/' + v.container_img}
-            alt=""
-            className="trans"
-          />
-          <span className="name">{v.pro_name}</span>
-          <small>${v.pro_price}</small>
+      <React.Fragment key={i}>
+        <div className="gift_sake" key={i}>
+          <div
+            className="sake_card"
+            onClick={() => {
+              setId(v.pro_id)
+              showHandle()
+            }}
+          >
+            <img
+              src={'http://localhost:3001/images/pro_img/' + v.pro_img}
+              alt=""
+              className="original"
+            />
+            <img
+              src={'http://localhost:3001/images/con_img/' + v.container_img}
+              alt=""
+              className="trans"
+            />
+            <span className="name">{v.pro_name}</span>
+            <small>${v.pro_price}</small>
+          </div>
+          <div
+            onClick={() => {
+              setCurrentId(v.pro_id)
+              setCurrentName(v.pro_name)
+              setCurrentPrice(v.pro_price)
+            }}
+          >
+            <SakeButton
+              sakeId={sakeId}
+              setSakeId={setSakeId}
+              sakeId2={sakeId2}
+              setSakeId2={setSakeId2}
+              name={name}
+              setName={setName}
+              name2={name2}
+              setName2={setName2}
+              price={price}
+              setPrice={setPrice}
+              price2={price2}
+              setPrice2={setPrice2}
+              conName={conName}
+              setConName={setConName}
+              currentPrice={currentPrice}
+              currentName={currentName}
+              currentId={currentId}
+              count={count}
+              setCount={setCount}
+            />
+          </div>
         </div>
-        <SakeButton />
-      </div>
+      </React.Fragment>
     )
   })
 
   return (
     <>
       {modalShow ? (
-        <ProductModal
-          modalShow={modalShow}
-          setModalShow={setModalShow}
-          sake={sake}
-        />
+        <ProductModal modalShow={modalShow} closeHandle={closeHandle} id={id} />
       ) : (
         ''
       )}
