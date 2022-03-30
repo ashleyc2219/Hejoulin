f7cbb5f9b6204d431b4a1726630945fde0cd77a3
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-03-30 09:02:23 +0800

    購物車酒標光箱

--- src/compenents/Cart/CartMarkModal.js
+++ src/compenents/Cart/CartMarkModal.js
@@ -6,7 +6,9 @@ const CartMarkModal = () => {
     <>
       <div className="CartMarkModal">
         <div className="markmodal">
-          <div className="img"></div>
+          <div className="img">
+            <img src="http://localhost:3001/images/mark_pic/1-1.jpg" alt="" />
+          </div>
           <div className="btns">
             <button className="btn-primary btn ">刪除</button>
             <button className="btn btn-secondary">確認</button>
