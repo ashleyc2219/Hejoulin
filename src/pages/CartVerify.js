676cf72c0236e4e71dd3c6f2a3b65d4a49f8700e
Ashley Chen <ashleyc2219@gmail.com>
2022-04-03 18:15:29 +0800

    完成驗證功能

--- src/pages/CartVerify.js
+++ src/pages/CartVerify.js
@@ -2,9 +2,63 @@ import React from 'react'
 import './../styles/CartVerify/CartVerify.scss'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CartVerifyInfo } from './../App'
+import { createBrowserHistory } from 'history'
 
 const CartVerify = () => {
-  const [btnText, setBtnText] = useState('取得簡訊傳送交易密碼')
+  let cartVerifyInfo = CartVerifyInfo._currentValue
+  console.log({
+    to: cartVerifyInfo.email_account,
+    cardNum: cartVerifyInfo.cardNum,
+    total: cartVerifyInfo.total,
+  })
+  const [btnText, setBtnText] = useState('取得email傳送交易密碼')
+  const [enterCode, setEnterCode] = useState('')
+
+  const today = new Date()
+  let date =
+    today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate()
+
+  const history = createBrowserHistory()
+
+  const sendEmail = async function () {
+    let data = {
+      email_account: cartVerifyInfo.email_account,
+      cardNum: cartVerifyInfo.cardNum,
+      total: cartVerifyInfo.total,
+    }
+
+    const r1 = await fetch(`http://localhost:3001/api/cart-verify/send-email`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    const obj = await r1.json()
+    console.log(obj)
+  }
+  const verifyTheCode = async function () {
+    let data = {
+      enterCode: enterCode,
+    }
+
+    const r1 = await fetch(
+      `http://localhost:3001/api/cart-verify/verify-code`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
+    )
+    const obj = await r1.json()
+    return obj.result
+  }
+
   return (
     <>
       <div className="CreditCardVerified">
@@ -24,22 +78,34 @@ const CartVerify = () => {
               </div>
               <div className="right">
                 <p>Hejoulin</p>
-                <p className="price">TWD 16888</p>
-                <p>2022/4/13</p>
-                <p>XXXX-XXXX-XXXX-1688</p>
+                <p className="price">TWD {cartVerifyInfo.total}</p>
+                <p>{date}</p>
+                <p>XXXX-XXXX-XXXX-{cartVerifyInfo.cardNum}</p>
                 <div className="input">
                   <input
                     type="text"
                     name=""
                     id=""
                     placeholder="請輸入驗證密碼"
+                    onChange={(e) => {
+                      setEnterCode(e.target.value)
+                    }}
                   />
                   <Link to="/cart/order">
-                    <button>確認</button>
+                    <button
+                      onClick={() => {
+                        verifyTheCode()
+                          ? history.push('/cart/order', { some: 'state' })
+                          : setBtnText('驗證碼錯誤 按我重新發送')
+                      }}
+                    >
+                      確認
+                    </button>
                   </Link>
                 </div>
                 <button
                   onClick={() => {
+                    sendEmail()
                     setBtnText('已將交易密碼送至信箱')
                   }}
                 >
@@ -57,7 +123,7 @@ const CartVerify = () => {
               <p>注意事項</p>
               <ol>
                 <li>
-                  請點選「取得簡訊傳送交易密碼」按鍵，本行將於1~2分鐘內以簡訊及E-mail傳送動態交易認證碼。
+                  請點選「取得email傳送交易密碼」按鍵，本行將於1~2分鐘內以簡訊及E-mail傳送動態交易認證碼。
                 </li>
                 <li>
                   請檢視留存本行手機或E-mail已取得動態交易認證密碼並輸入送出。
