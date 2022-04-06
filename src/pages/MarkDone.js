b0281675f38a0d767993b2741f0f579cc2fae5e0
Mary Hsieh <a5925457@gmail.com>
2022-04-06 14:41:54 +0800

    update mark

--- src/pages/MarkDone.js
+++ src/pages/MarkDone.js
@@ -16,6 +16,15 @@ const MarkDone = () => {
       setMarkImg([]) // This worked for me
     }
   }, [])
+
+  const downloadImg = () => {
+    let img = document.createElement('a')
+    img.href = 'http://localhost:3001/images/mark_pic/' + markImg[0]?.pics
+    img.download = 'mark.png'
+    document.body.appendChild(img)
+    img.click()
+    document.body.removeChild(img)
+  }
   return (
     <>
       <div className="MarkDone">
@@ -24,24 +33,32 @@ const MarkDone = () => {
             <div className="fake"></div>
           </div>
           <div className="done">
-            <img src="/Mark/1.png" alt="" className="bottle image" />
             {markImg.length !== 0 && (
-              <>
+              <div className="img-show">
+                <img src="/Mark/1.png" alt="" className="bottle image" />
                 <img
                   src={
                     'http://localhost:3001/images/mark_pic/' + markImg[0]?.pics
                   }
                   alt=""
-                  className="sticker image"
+                  className="sticker image "
                 />
                 <div className="shadow"></div>
-              </>
+              </div>
             )}
           </div>
+
           <div className="grid">
             <div className="group">
               <p className="text">想分享圖片嗎？可以先進行下載</p>
-              <button className="btn btn-primary">下載圖片</button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  downloadImg()
+                }}
+              >
+                下載圖片
+              </button>
             </div>
             <div className="group">
               <p className="text">還想做其他的專屬酒標？那就…</p>
@@ -51,17 +68,28 @@ const MarkDone = () => {
             </div>
             <div className="group">
               <p className="text">想擁有實體只屬於自己的酒！</p>
-              <button className="btn btn-primary">前往商城</button>
+              <Link to="/product/list">
+                <button className="btn btn-primary">前往商城</button>
+              </Link>
             </div>
           </div>
           <div className="mobile">
             <div className="download">
               <p>想分享圖片嗎？</p>
-              <button className="btn btn-primary">下載圖片</button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  downloadImg()
+                }}
+              >
+                下載圖片
+              </button>
             </div>
             <div className="turn">
               <p>先儲存圖片，接著…</p>
-              <button className="shop btn btn-secondary">前往購物</button>
+              <Link to="/product/list">
+                <button className="shop btn btn-secondary">前往購物</button>
+              </Link>
               <span>或</span>
               <Link to="/mark/edit">
                 <button className="btn btn-success next">製作下一張</button>
