84bd6df9d7f78b6ea050920511c1aafc0971c3cf
Ashley Chen <ashleyc2219@gmail.com>
2022-04-06 17:16:10 +0800

    接localstorage 的資料

--- src/compenents/Sub/SubCartInfoCard.js
+++ src/compenents/Sub/SubCartInfoCard.js
@@ -1,21 +1,57 @@
 import React from 'react'
 import './SubCartInfoCard.scss'
 
-const SubCartInfoCard = () => {
+const SubCartInfoCard = (props) => {
+  const { subPlan, subMonth } = props
+
+  function calPrice() {
+    if (subPlan === '純米') {
+      return 1300
+    }
+    if (subPlan === '純米吟釀') {
+      return 1500
+    }
+    if (subPlan === '純米大吟釀') {
+      return 1800
+    }
+  }
+  function calTotal() {
+    if (subMonth === 1) {
+      return calPrice() * subMonth
+    }
+    if (subMonth === 6) {
+      return calPrice() * subMonth * 0.85
+    }
+    if (subMonth === 12) {
+      return calPrice() * subMonth * 0.8
+    }
+  }
+  function planImg() {
+    if (subPlan === '純米') {
+      return 1
+    }
+    if (subPlan === '純米吟釀') {
+      return 2
+    }
+    if (subPlan === '純米大吟釀') {
+      return 3
+    }
+  }
+  let imgFile = planImg()
   return (
     <>
       <div className="table-item sub-item">
         <div className="item item-product-img">
-          <img src="/Sub/rice_1.svg" alt="" />
+          <img src={`/Sub/rice_${imgFile}.svg`} alt="" />
         </div>
         <div className="item item-product-info">
-          <h5>純米吟釀</h5>
+          <h5>{subPlan}</h5>
 
-          <p className="price">1500</p>
-          <p className="month">12</p>
+          <p className="price">{calPrice()}</p>
+          <p className="month">{subMonth}</p>
         </div>
         <div className="item item-subtotal">
-          <p>11520</p>
+          <p>{calTotal()}</p>
         </div>
       </div>
       <hr />
