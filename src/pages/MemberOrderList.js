c3fbf9203d70ee1c28c16b320f8d78fb9b4e8243
Wilson#122 <cwhth2956@gmail.com>
2022-04-07 21:12:16 +0800

    修整程式碼

--- src/pages/MemberOrderList.js
+++ src/pages/MemberOrderList.js
@@ -3,17 +3,14 @@ import '../styles/Member/MemberOrderList.scss'
 import Sidebar from '../compenents/Member/SideBar/Sidebar'
 import TabsOrder from '../compenents/Member/AllTabs/OrderList/TabsOrder'
 
-const MemberOrderList = ({ detailData, setDetailData }) => {
+const MemberOrderList = () => {
 
     return (
         <>
             <div className="MemberOrderList">
                 <Sidebar/>
                 <div className="MemberOrderBox">
-                    <TabsOrder
-                        detailData={detailData}
-                        setDetailData={setDetailData}
-                    />
+                    <TabsOrder />
                 </div>
             </div>
         </>
