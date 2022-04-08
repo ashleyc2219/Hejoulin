31ec90166dbc48f9b5507d41bc73ef4c7120be9f
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-04-08 16:56:14 +0800

    解決無法關閉手機版分類選單的bug

--- src/compenents/ProductList/MobileCatModal.js
+++ src/compenents/ProductList/MobileCatModal.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './../../styles/ProductList/MobileCatModal.css'
 
-const MobileCatModal = ({ setCatModal, catModal }) => {
+const MobileCatModal = ({ mobilecat, catModal }) => {
   return (
     <>
       <div className="MobileCatModal">
         <div className="cat-modal">
           <img
             onClick={() => {
-              setCatModal(!catModal)
+              mobilecat()
             }}
             src="/ProductList/close-black.svg"
             alt=""
             className="close-black"
           />
           <div className="title">
-            <span></span>排序
+            <span></span>分類
           </div>
           <div className="cat">
             <ul>
