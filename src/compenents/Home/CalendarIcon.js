7c5c4803f59b3d7414ec1d43f3d28589c1a4192a
grace chuang <grace350276@gmail.com>
2022-04-09 17:05:12 +0800

    解決年領警示bug

--- src/compenents/Home/CalendarIcon.js
+++ src/compenents/Home/CalendarIcon.js
@@ -14,26 +14,26 @@ const CalendarIcon = () => {
         <path
           d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z"
           stroke="#3D4349"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16 2V6"
           stroke="#3D4349"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M8 2V6"
           stroke="#3D4349"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M3 10H21"
           stroke="#3D4349"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </>
