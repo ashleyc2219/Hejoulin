e2edf19f517a66e15dd6b2a5fec87f9aa03ea69f
grace chuang <grace350276@gmail.com>
2022-04-11 14:15:01 +0800

    首頁 開啟輪播

--- src/compenents/Home/Carousel.js
+++ src/compenents/Home/Carousel.js
@@ -16,27 +16,27 @@ class Carousel extends React.Component {
     this.leftClick = this.moveLeft.bind(this)
     this.childClickChange = this.childClickChange.bind(this)
 
-    // this.start = this.start.bind(this)
-    // this.pause = this.pause.bind(this)
+    this.start = this.start.bind(this)
+    this.pause = this.pause.bind(this)
   }
 
-  // componentDidMount() {
-  //   this.timeId = setInterval(this.rightClick, 2500)
-  //   this.setState({ interval: this.timeId })
-  // }
+  componentDidMount() {
+    this.timeId = setInterval(this.rightClick, 2500)
+    this.setState({ interval: this.timeId })
+  }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.timeId)
-  // }
+  componentWillUnmount() {
+    clearInterval(this.timeId)
+  }
 
-  // start() {
-  //   this.timeId = setInterval(this.rightClick, 2500)
-  //   this.setState({ interval: this.timeId })
-  // }
+  start() {
+    this.timeId = setInterval(this.rightClick, 2500)
+    this.setState({ interval: this.timeId })
+  }
 
-  // pause() {
-  //   clearInterval(this.state.interval)
-  // }
+  pause() {
+    clearInterval(this.state.interval)
+  }
 
   generateItems() {
     const items = []
