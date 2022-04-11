40e7085126486cae8e1b6c7d2fa4fd5e41cfc82d
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-04-11 19:01:19 +0800

    修改css

--- src/compenents/Member/Mark/MarkData.js
+++ src/compenents/Member/Mark/MarkData.js
@@ -1,52 +1,50 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import MarkModal from './MarkModal'
 import '../../../styles/Member/Member-Mark/MarkData.scss'
-import MarkImgItem from "./MarkImgItem";
-import {Link} from "react-router-dom";
+import MarkImgItem from './MarkImgItem'
+import { Link } from 'react-router-dom'
 
 const MarkData = (props) => {
-    const {markData, whichMark, setWhichMark} = props
-    const [modalShow, setModalShow] = useState(false)
-    const [modalContent, setModalContent] = useState('')
+  const { markData, whichMark, setWhichMark } = props
+  const [modalShow, setModalShow] = useState(false)
+  const [modalContent, setModalContent] = useState('')
 
-    const renderImg =  (markData) => {
-        if (markData && markData.length) {
-              return  <MarkImgItem
-                    setModalShow={setModalShow}
-                    markData={markData}
-                    setModalContent={setModalContent}
-                    whichMark={whichMark}
-                    setWhichMark={setWhichMark}
-                />
-        } else {
-            return null
-        }
+  const renderImg = (markData) => {
+    if (markData && markData.length) {
+      return (
+        <MarkImgItem
+          setModalShow={setModalShow}
+          markData={markData}
+          setModalContent={setModalContent}
+          whichMark={whichMark}
+          setWhichMark={setWhichMark}
+        />
+      )
+    } else {
+      return null
     }
+  }
 
-    return (
-        <>
-            <div className="MarkItem">
-                <div className="MarkItemAdd">
-                    <Link to="/mark/intro">
-                        <div className="MarkPicAdd">
-                            <img src="/Member/plusCircle.svg" alt=""/>
-                        </div>
-                        <div className="MarkAddText">立即新增您的專屬酒標</div>
-                    </Link>
-                </div>
-                {markData.length > 0 ? (
-                    renderImg(markData)
-                ) : (
-                    ''
-                )}
-                {modalShow ? (
-                    <MarkModal setModalShow={setModalShow} modalContent={modalContent} />
-                ) : (
-                    ''
-                )}
+  return (
+    <>
+      <div className="MarkItem">
+        <div className="MarkItemAdd">
+          <Link to="/mark/intro">
+            <div className="MarkPicAdd">
+              <img src="/Member/plusCircle.svg" alt="" />
             </div>
-        </>
-    )
+            <div className="MarkAddText">立即新增您的專屬酒標</div>
+          </Link>
+        </div>
+        {markData.length > 0 ? renderImg(markData) : ''}
+        {modalShow ? (
+          <MarkModal setModalShow={setModalShow} modalContent={modalContent} />
+        ) : (
+          ''
+        )}
+      </div>
+    </>
+  )
 }
 
 export default MarkData
--- src/pages/MemberMark.js
+++ src/pages/MemberMark.js
@@ -1,96 +1,97 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../compenents/Member/SideBar/Sidebar'
 import '../styles/Member/MenberMark.scss'
 import MarkModal from '../compenents/Member/Mark/MarkModal'
 import MarkData from '../compenents/Member/Mark/MarkData'
-import { useHistory} from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 
 const MemberMark = () => {
-    const [markData, setMarkData] = useState([])
-    const [whichMark, setWhichMark] = useState([])
-    const APIDelMark = "http://localhost:3001/user/member/MemberMarkDelete"
-    const APIMark = "http://localhost:3001/user/member/MemberMark"
-    const history = useHistory()
+  const [markData, setMarkData] = useState([])
+  const [whichMark, setWhichMark] = useState([])
+  const APIDelMark = 'http://localhost:3001/user/member/MemberMarkDelete'
+  const APIMark = 'http://localhost:3001/user/member/MemberMark'
+  const history = useHistory()
 
-    useEffect(() => {
-        ;(async () => {
-            const res = await fetch(APIMark,{
-                    method: 'POST',
-                    headers: {
-                        Authorization: 'Bearer ' + localStorage.token,
-                    },
-                }
-            )
-            const obj = await (await res).json()
-            console.log(obj)
-            setMarkData(obj)
-        })()
-    }, [])
+  useEffect(() => {
+    ;(async () => {
+      const res = await fetch(APIMark, {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + localStorage.token,
+        },
+      })
+      const obj = await (await res).json()
+      console.log(obj)
+      setMarkData(obj)
+    })()
+  }, [])
 
-    const deleteMark = async () => {
-        const data = {
-            mark_id: whichMark,
-        }
-        console.log(data)
-        const settings = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + localStorage.token,
-            },
-            body: JSON.stringify(data),
-        }
-        try {
-            const fetchResponse = await fetch(APIDelMark, settings)
-            const dataRs = await fetchResponse.json()
-            console.log(dataRs)
-            if (dataRs.success === true) {
-                history.go(0)
-            }
-            //setMarkData(data)
-        } catch (err) {
-            return err
-        }
+  const deleteMark = async () => {
+    const data = {
+      mark_id: whichMark,
     }
-
-    let active = true
-    const click = () => {
-        if (active === true) {
-            ;(async function del() {
-                await deleteMark(whichMark)
-            })()
-
-            active = false
-        }
+    console.log(data)
+    const settings = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.token,
+      },
+      body: JSON.stringify(data),
     }
-    return (
-        <>
-            <div className="MemberMark">
-                <Sidebar/>
-                <div className="MemberMarkBox">
-                    <div className="container">
-                        <div className="row MemberMarkBar">
-                            <button className="btn btn-primary btn-delete" onClick={click}>
-                                刪除所選酒標
-                            </button>
-                        </div>
-                    </div>
-                    <div className="MemberMarkItems">
-                        <div className="bgImg">
-                            <img src="/Member/MarkBgImg.svg" alt=""/>
-                        </div>
-                        <MarkData
-                            markData={markData}
-                            setMarkData={setMarkData}
-                            whichMark={whichMark}
-                            setWhichMark={setWhichMark}
-                        />
-                        <img src="/Member/MarkBg-SAKE.png" className="SakeBg" alt=""/>
-                    </div>
-                </div>
+    try {
+      const fetchResponse = await fetch(APIDelMark, settings)
+      const dataRs = await fetchResponse.json()
+      console.log(dataRs)
+      if (dataRs.success === true) {
+        history.go(0)
+      }
+      //setMarkData(data)
+    } catch (err) {
+      return err
+    }
+  }
+
+  let active = true
+  const click = () => {
+    if (active === true) {
+      ;(async function del() {
+        await deleteMark(whichMark)
+      })()
+
+      active = false
+    }
+  }
+  return (
+    <>
+      <div className="MemberMark">
+        <Sidebar />
+        <div className="MemberMarkBox">
+          <div className="container">
+            <div className="row MemberMarkBar">
+              <button className="btn btn-primary btn-delete" onClick={click}>
+                刪除所選酒標
+              </button>
             </div>
-        </>
-    )
+          </div>
+          <div className="MemberMarkItems">
+            {/* <div className="bgImg">
+              <img src="/Member/MarkBgImg.svg" alt="" />
+            </div> */}
+            <MarkData
+              markData={markData}
+              setMarkData={setMarkData}
+              whichMark={whichMark}
+              setWhichMark={setWhichMark}
+            />
+            <div className="sake">
+              <img src="/Member/MarkBg-SAKE.png" className="SakeBg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default MemberMark
